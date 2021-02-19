e7030f308e09cc6b20558984e75095e4b933c55e
Javier Pardo <47202605+JaviPardox@users.noreply.github.com>
2021-02-18 22:46:06 -0800

    now managers don't show up on employees queries, add comments

--- EmployeeTableQueries.sql
+++ EmployeeTableQueries.sql
@@ -1,28 +1,75 @@
 
+-- This section is to create a table that holds information about the employees, leaving the managers out	
 DROP TABLE employee_details;
 
+-- Table that holds all the employees information, including managers
 CREATE TABLE employee_details AS
 	SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 	FROM employees
+	INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 	INNER JOIN salaries ON employees.emp_no=salaries.emp_no
 	ORDER BY emp_no ASC;
 
+-- Proceed to delete all the rows with matching employee's numbers between managers and employees in order to delete all the manager's rows.
+DELETE 
+FROM employee_details 
+USING dept_manager 
+WHERE employee_details.emp_no = dept_manager.emp_no;
+
+---------------------------
+
+-- This section is to find all employees, leaving all managers out, that are hired in 1986
 DROP TABLE employee_1986;
 
+-- Find all employees hired in the year 1986
 CREATE TABLE employee_1986 AS 
-	SELECT employees.first_name, employees.last_name, employees.hire_date
+	SELECT employees.first_name, employees.last_name, employees.hire_date, employees.emp_no
 	FROM employees
-	WHERE hire_date >= '1986-01-01'::date AND hire_date <= '1986-12-31'::date;
-	
+	WHERE hire_date >= '1986-01-01'::date AND hire_date <= '1986-12-31'::date
+	ORDER BY hire_date ASC;
+
+-- Delete all managers from the list by comparing emp_no
+DELETE 
+FROM employee_1986 
+USING dept_manager 
+WHERE employee_1986.emp_no = dept_manager.emp_no;
+
+-- Drop emp_no column because it's not needed
+ALTER TABLE employee_1986
+DROP COLUMN emp_no;
+
+---------------------------
+
+-- This section is to find all manager's information
 DROP TABLE managers;	
 
+-- Find all managers information and store it into a table
 CREATE TABLE managers AS
-	SELECT employees.emp_no, employees.last_name, employees.first_name, dept_manager.dept_no, departments.dept_name
+	SELECT dept_manager.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.first_name  
 	FROM employees
 	INNER JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
 	INNER JOIN departments ON dept_manager.dept_no=departments.dept_no
 	ORDER BY emp_no ASC;
 
-SELECT * FROM managers;
+---------------------------
 
+-- This section shows the employee's department information, plus first and last name
+DROP TABLE employee_department;
 
+-- Create table with all the information
+CREATE TABLE employee_department AS
+	SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+	FROM employees
+	INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+	INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
+	ORDER BY emp_no ASC;
+
+-- Delete all managers from the list by comparing emp_no
+DELETE 
+FROM employee_department 
+USING dept_manager 
+WHERE employee_department.emp_no = dept_manager.emp_no;
+	
+---------------------------	
+	
+--SELECT * FROM employee_department;
